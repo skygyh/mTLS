2163402895fe68bc79ee754bfd93ccee260da19c
gongy2 <yunhai.gong@dell.com>
2024-02-07 16:05:04 +0800

    reload

--- reload_server.go
+++ reload_server.go
@@ -77,10 +77,22 @@ func reload_TLSConfig(server http.Server) error {
 
 	// Create a new TLS configuration with the new certificate and key
 	newTLSConfig := &tls.Config{
+		ClientCAs: caCertPool,
 		GetCertificate: func(*tls.ClientHelloInfo) (*tls.Certificate, error) {
 			return &newCert, nil
 		},
-		RootCAs: caCertPool,
+		ClientAuth:               tls.RequireAndVerifyClientCert,
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		},
 	}
 
 	// Atomically swap the TLS configuration pointer
