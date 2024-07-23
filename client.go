7cc2d4e104df53d810b0ffcd536a2f5af366e73b
gongy2 <yunhai.gong@dell.com>
2024-07-23 16:07:54 +0800

    update client

--- client.go
+++ client.go
@@ -11,28 +11,24 @@ import (
 	"time"
 )
 
-const ca_cert_dir string = "./certs.bk"
+const CertsDir string = "/etc/tls"
 
 func main() {
 
-	name := flag.String("c", "a", "client name")
+	address := flag.String("link", "", "link address")
 	flag.Parse()
 
-	clientCaCert := fmt.Sprintf("%s/ca.crt", ca_cert_dir)
-	log.Println("Load CA- ", clientCaCert)
-	cert, err := ioutil.ReadFile(clientCaCert)
+	// load CA certificate file and add it to list of client CAs
+	caCertFile, err := ioutil.ReadFile(CertsDir + "/ca.crt")
 	if err != nil {
-		log.Fatalf("could not open certificate file: %v", err)
+		log.Fatalf("error reading CA certificate: %v", err)
 	}
 	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(cert)
+	caCertPool.AppendCertsFromPEM(caCertFile)
 
-	clientCert := fmt.Sprintf("%s/client.%s.crt", ca_cert_dir, *name)
-	clientKey := fmt.Sprintf("%s/client.%s.key", ca_cert_dir, *name)
-	log.Println("Load key pairs - ", clientCert, clientKey)
-	certificate, err := tls.LoadX509KeyPair(clientCert, clientKey)
+	serverCert, err := tls.LoadX509KeyPair(CertsDir+"/tls.crt", CertsDir+"/tls.key")
 	if err != nil {
-		log.Fatalf("could not load certificate: %v", err)
+		log.Fatalf("error loading server certificate and key: %v", err)
 	}
 
 	client := http.Client{
@@ -40,7 +36,7 @@ func main() {
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{
 				RootCAs:      caCertPool,
-				Certificates: []tls.Certificate{certificate},
+				Certificates: []tls.Certificate{serverCert},
 			},
 		},
 	}
@@ -51,7 +47,7 @@ func main() {
 	//   --cacert ./ca.crt  --cert ./client.b.crt --key ./client.b.key  \
 	//     https://localhost:8443/hello
 
-	r, err := client.Get("https://localhost:9443/hello")
+	r, err := client.Get(*address)
 	if err != nil {
 		log.Fatalf("error making get request: %v", err)
 	}
